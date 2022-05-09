8b5a7e419cc4a659cae4459763e4fc1e8251faf6
MOHIT PERIWAL <84000709+MohitP-2002@users.noreply.github.com>
2022-05-09 10:30:19 +0530

    Added solution - LeetHub

---
+++ Boolean Matrix - GFG/boolean-matrix.cpp
@@ -0,0 +1,73 @@
+// { Driver Code Starts
+#include <bits/stdc++.h> 
+using namespace std; 
+
+ // } Driver Code Ends
+
+
+class Solution
+{   
+    public:
+    //Function to modify the matrix such that if a matrix cell matrix[i][j]
+    //is 1 then all the cells in its ith row and jth column will become 1.
+    void booleanMatrix(vector<vector<int> > &matrix)
+    {
+        // code here 
+        vector<pair<int,int>> v;
+        int m=matrix.size(); 
+        int n=matrix[0].size();
+        for(int i=0;i<m;i++){
+            for(int j=0;j<n;j++){
+                if(matrix[i][j]==1){
+                    v.push_back(make_pair(i,j));
+                }
+            }
+        }
+        for(auto s:v){
+            for(int i=0;i<n;i++){
+                matrix[s.first][i]=1;
+            }
+            for(int i=0;i<m;i++){
+                matrix[i][s.second]=1;
+            }
+        }
+    }
+};
+
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--) 
+    {
+        int row, col;
+        cin>> row>> col;
+        vector<vector<int> > matrix(row); 
+        for(int i=0; i<row; i++)
+        {
+            matrix[i].assign(col, 0);
+            for( int j=0; j<col; j++)
+            {
+                cin>>matrix[i][j];
+            }
+        }
+        
+        Solution ob;
+        ob.booleanMatrix(matrix);
+
+
+        for (int i = 0; i < row; ++i)
+        {
+            for (int j = 0; j < col; ++j)
+            {
+                cout<<matrix[i][j]<<" ";
+            }
+            cout<<endl;
+        }
+    }
+    return 0;
+}
+
+
+  // } Driver Code Ends
