6757a5886276c2a0b04f983156c9fbb6018da90e
MOHIT PERIWAL <84000709+MohitP-2002@users.noreply.github.com>
2022-05-10 17:54:20 +0530

    Added solution - LeetHub

---
+++ Help Classmates - GFG/help-classmates.cpp
@@ -0,0 +1,58 @@
+// { Driver Code Starts
+//Initial Template for C++
+
+#include <bits/stdc++.h> 
+using namespace std; 
+
+
+ // } Driver Code Ends
+//User function Template for C++
+
+class Solution{
+    
+    public:
+    vector<int> help_classmate(vector<int> arr, int n) 
+    { 
+        // Your code goes here
+        //montonic stack to find next smaller or previous smaller element in an array
+        vector<int>  v(n,-1);
+        stack<int> s;
+        for(int i=0;i<n;i++){
+            while(!s.empty() && arr[s.top()]>arr[i]){
+                auto x = s.top();
+                s.pop();
+                v[x]=arr[i];
+            }
+            s.push(i);
+        }
+        return v;
+    } 
+};
+
+// { Driver Code Starts.
+
+int main() 
+{ 
+	int t;
+	cin>>t;
+	while(t--)
+	{
+		int n;
+		cin>>n;
+		vector<int> array(n);
+		for (int i = 0; i < n; ++i)
+		{
+			cin>>array[i];
+		}
+		Solution obj;
+		vector<int> result = obj.help_classmate(array,n);
+		for (int i = 0; i < n; ++i)
+		{
+			cout<<result[i]<<" ";
+		}
+		cout<<"\n";
+	}
+	return 0; 
+}
+
+  // } Driver Code Ends
