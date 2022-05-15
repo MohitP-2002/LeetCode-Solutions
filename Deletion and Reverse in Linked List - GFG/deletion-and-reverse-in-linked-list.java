3c27f48c57d15a54e40950759e19eb943ee2aecc
MOHIT PERIWAL <84000709+MohitP-2002@users.noreply.github.com>
2022-05-15 10:48:38 +0530

    Added solution - LeetHub

---
+++ Deletion and Reverse in Linked List - GFG/deletion-and-reverse-in-linked-list.java
@@ -0,0 +1,104 @@
+// { Driver Code Starts
+import java.util.Scanner;
+import java.io.*;
+
+class Node
+{
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data=d;
+        next=null;
+    }
+}
+class GFG1 {
+    public static void printList(Node head)
+    {
+        Node start=head;
+        System.out.print(head.data+" ");
+        head=head.next;
+        while(head!=start)
+        {
+            System.out.print(head.data+" ");
+            head=head.next;
+        }
+        System.out.println();
+    }
+	public static void main (String[] args) {
+	    Scanner sc=new Scanner(System.in);
+	    int t=sc.nextInt();
+	    while(t-->0)
+	    {
+	        int n=sc.nextInt();
+	        Node start=null,cur=null;
+	        for(int i=0;i<n;i++)
+	        {
+	            int a=sc.nextInt();
+	            Node ptr=new Node(a);
+	            if(start==null)
+	            {
+	                start=ptr;
+	                cur=ptr;
+	            }
+	            else
+	            {
+	                cur.next=ptr;
+	                cur=ptr;
+	            }
+	        }
+	        int delNode=sc.nextInt();
+	        cur.next=start;
+	        GFG obj=new GFG();
+	        Node head=obj.deleteNode(start,delNode);
+	        start=obj.reverse(head);
+	        printList(start);
+	    }
+	}
+}// } Driver Code Ends
+
+
+/*class Node
+{
+    int data;
+    Node next;
+    Node(int d)
+    {
+        data=d;next=null;
+    }
+}*/
+class GFG
+{
+    //This method returns the head of the LL after deleting node with value d.
+    public static Node deleteNode(Node head,int d)
+    {
+        //Add your code here.
+        Node n =head, prev=null;
+        while(n!=null){
+            if(n.data==d){
+                prev.next=n.next;
+                break;
+            }
+            prev=n;
+            n=n.next;
+        }
+        return head;
+    }
+    //This method returns the head node of the reversed Linked list.
+    public static Node reverse(Node head)
+    {
+        //Add your code here
+        Node current,next,prev;
+        current=head;
+        prev=null;
+        while(current!=null)
+        {
+            next=current.next;
+            current.next=prev;
+            prev=current;
+            current=next;
+        }
+        head=prev.next;
+        return head;
+    }
+}
