493653161eb07940ac3a40039180b248bb8d3583
MOHIT PERIWAL <84000709+MohitP-2002@users.noreply.github.com>
2022-05-28 11:36:04 +0530

    Time: 646 ms (14.19%), Space: 6.7 MB (82.35%) - LeetHub

--- 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
+++ 132-palindrome-partitioning-ii/132-palindrome-partitioning-ii.cpp
@@ -1,16 +1,30 @@
 class Solution {
 public:
-    int minCut(string s) {
-        int n = s.size();
-        vector<int> cut(n+1, 0);  // number of cuts for the first k characters
-        for (int i = 0; i <= n; i++) cut[i] = i-1;
-        for (int i = 0; i < n; i++) {
-            for (int j = 0; i-j >= 0 && i+j < n && s[i-j]==s[i+j] ; j++) // odd length palindrome
-                cut[i+j+1] = min(cut[i+j+1],1+cut[i-j]);
-
-            for (int j = 1; i-j+1 >= 0 && i+j < n && s[i-j+1] == s[i+j]; j++) // even length palindrome
-                cut[i+j+1] = min(cut[i+j+1],1+cut[i-j+1]);
+    bool isPalindrome(int i, int j, string& str){
+        while(i<j){
+            if(str[i]!=str[j]) return false;
+            i++;
+            j--;
         }
-        return cut[n];
+        return true;
+    }
+    int fun(int i, int n, string& str, vector<int>& dp){
+        //base case 
+        if(i==n) return 0;
+        //memoization
+        if(dp[i]!=-1) return dp[i];
+        int minCost=INT_MAX;
+        for(int j=i;j<n;j++){
+            if(isPalindrome(i,j,str)){
+                int cost=1+fun(j+1,n,str, dp);
+                minCost=min(minCost,cost);
+            }
+        }
+        return dp[i]=minCost;
+    }
+    int minCut(string s) {
+        int n =s.length();
+        vector<int> dp(n,-1);
+        return fun(0,n,s,dp)-1;
     }
 };
