343cc2b9e9c008a0c91892bb2cdb406e4a7dcd2e
MOHIT PERIWAL <84000709+MohitP-2002@users.noreply.github.com>
2022-06-11 15:39:18 +0530

    Time: 15 ms (56.40%), Space: 7.7 MB (46.56%) - LeetHub

---
+++ 13-roman-to-integer/13-roman-to-integer.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    int romanToInt(string s) {
+        if (s.empty()) return 0;
+        
+        unordered_map<char, int> table = {
+            {'I', 1},
+            {'V', 5},
+            {'X', 10},
+            {'L', 50},
+            {'C', 100},
+            {'D', 500},
+            {'M', 1000}
+        };
+        int sum=0;
+        for(int i=0;i<s.length()-1;i++){
+            if(table[s[i]]<table[s[i+1]]) sum-=table[s[i]];
+            else sum+=table[s[i]];
+        }
+        sum+=table[s.back()];
+        return sum;
+    }
+};
