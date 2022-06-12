a07e4fdc02e715b0d3a45b48b42af52851a58e8f
MOHIT PERIWAL <84000709+MohitP-2002@users.noreply.github.com>
2022-06-12 16:35:54 +0530

    Added solution - LeetHub

---
+++ Swap Kth nodes from ends - GFG/swap-kth-nodes-from-ends.cpp
@@ -0,0 +1,110 @@
+// { Driver Code Starts
+#include <iostream>
+
+using namespace std;
+
+struct Node {
+  int data;
+  struct Node *next;
+  Node(int x) {
+    data = x;
+    next = NULL;
+  }
+};
+
+
+
+Node *swapkthnode(Node* head, int num, int K);
+
+void addressstore(Node **arr, Node* head)
+{
+    Node* temp = head;
+    int i = 0;
+    while(temp){
+        arr[i] = temp;
+        i++;
+        temp = temp->next;
+    }
+}
+
+bool check(Node ** before, Node **after, int num, int K)
+{
+    if(K > num)
+        return 1;
+    return (before[K-1] == after[num - K]) && (before[num-K] == after[K-1]);
+}
+
+int main()
+{
+    int T;
+    cin>>T;
+    while(T--){
+        int num, K , firstdata;
+        cin>>num>>K;
+        int temp;
+        cin>>firstdata;
+        Node* head = new Node(firstdata);
+        Node* tail = head;
+        for(int i = 0; i<num - 1; i++){
+            cin>>temp;
+            tail->next = new Node(temp);
+            tail = tail->next;
+        }
+        
+        Node *before[num];
+        addressstore(before, head);
+        
+        head = swapkthnode(head, num, K);
+        
+        Node *after[num];
+        addressstore(after, head);
+        
+        if(check(before, after, num, K))
+            cout<<1<<endl;
+        else
+            cout<<0<<endl;
+    }
+}
+// } Driver Code Ends
+
+
+//User function Template for C++
+
+/* Linked List Node structure
+   struct Node  {
+     int data;
+     Node *next;
+     Node(int x) {
+        data = x;
+        next = NULL;
+  }
+  }
+*/
+
+//Function to swap Kth node from beginning and end in a linked list.
+Node *swapkthnode(Node* head, int num, int k)
+{
+    // Your Code here
+    if(k>num) return head;
+    Node* first=head, *second=head, *prevf=nullptr, *prevs=nullptr;
+    for(int i=1;i<k;i++){
+        prevf=first;
+        first=first->next;
+    }
+    for(int i=1;i<num-k+1;i++){
+        prevs=second;
+        second=second->next;
+    }
+    if(prevf!=NULL){
+        prevf->next=second;
+    }
+    if(prevs!=NULL){
+        prevs->next=first;
+    }
+    Node* temp = first->next;
+    first->next = second->next;
+    second->next = temp;
+    if(k==1) head=second;
+    if(k==num) head=first;
+    return head;
+}
