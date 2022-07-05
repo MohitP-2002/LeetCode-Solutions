181d3212c2c4df2115225a0072da89809861e87c
MOHIT PERIWAL <84000709+MohitP-2002@users.noreply.github.com>
2022-07-05 12:37:58 +0530

    Time: 20 ms (50.81%), Space: 12.6 MB (49.14%) - LeetHub

--- 692-top-k-frequent-words/692-top-k-frequent-words.cpp
+++ 692-top-k-frequent-words/692-top-k-frequent-words.cpp
@@ -1,27 +1,31 @@
 class Solution {
 public:
+    struct Node{
+        string word;
+        int freq;
+        int index;
+    };
+    struct comp{
+        bool operator()(Node a, Node b){
+          return a.freq > b.freq || (a.freq == b.freq && a.word < b.word);
+        }
+    };
     vector<string> topKFrequent(vector<string>& words, int k) {
-        unordered_map<string, int> freq;
-        for(auto w : words){
-            freq[w]++;
+        vector<string> v;
+        unordered_map<string, int> m;
+        for(auto i:words) m[i]++;
+        priority_queue<Node, vector<Node>, comp> q;
+        for(auto i:m){
+            q.push({i.first,i.second});
+            if(q.size()>k){
+                q.pop();
+            }
         }
-        
-        auto comp = [&](const pair<string,int>& a, const pair<string,int>& b) {
-            return a.second > b.second || (a.second == b.second && a.first < b.first);
-        };
-        typedef priority_queue< pair<string,int>, vector<pair<string,int>>, decltype(comp) > my_priority_queue_t;
-        my_priority_queue_t  pq(comp);
-        
-        for(auto w : freq ){
-            pq.emplace(w.first, w.second);
-            if(pq.size()>k) pq.pop();
+        while(!q.empty()){
+            v.push_back(q.top().word);
+            q.pop();
         }
-        
-        vector<string> output;
-        while(!pq.empty()){
-            output.insert(output.begin(), pq.top().first);
-            pq.pop();
-        }
-        return output;
+        reverse(v.begin(),v.end());
+        return v;
     }
 };
